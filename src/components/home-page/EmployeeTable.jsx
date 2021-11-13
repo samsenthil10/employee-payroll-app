f46eb968415810fcaed21e9d55673da4a2cdee49
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-13 06:53:20 +0530

    Added: Components for Home page

---
+++ src/components/home-page/EmployeeTable.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import deleteIcon from '../../assets/icons/delete-black-18dp.svg'
+import editIcon from '../../assets/icons/create-black-18dp.svg'
+import profile from '../../assets/profile-images/Ellipse -3.png'
+import './HomePage.scss';
+
+const Display = (props) => {
+    
+    const remove = (id) => {
+        console.log("remove")
+    }
+
+    const update = (id) => {
+        console.log("update")
+
+
+    }
+
+
+    return (
+
+        <table id="display" className="table">
+            <tbody>
+                <tr key={-1}>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Gender</th>
+                    <th>Department</th>
+                    <th>Salary</th>
+                    <th>Start Date</th>
+                    <th>Actions</th>"
+
+                </tr>
+                {
+                    props.employeeArray && props.employeeArray.map((element, ind) => (
+                        <tr key={ind}>
+                            <td><img className="profile" src={profile} alt="" /></td>
+                            <td>{element.name}</td>
+                            <td>{element.gender}</td>
+                            <td><div className="depts">
+                                {element.departMentValue && element.departMentValue.map(dept => (
+                                    <div className="dept-label">{dept}</div>
+                                ))}
+                            </div></td>
+                            <td>{element.salary}</td>
+                            <td>{element.startDate}</td>
+                            <td>
+                                <img onClick={() => remove(element.id)} src={deleteIcon} alt="delete" />
+                                <img onClick={() => update(element.id)} src={editIcon} alt="edit" />
+                            </td>
+
+                        </tr>
+                    ))
+                }
+            </tbody>
+        </table>
+
+
+    )
+}
+
+export default Display
---
+++ src/components/home-page/HomePage.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import EmployeeService from '../../services/EmployeePayrollService';
+import EmployeeTable from './EmployeeTable';
+import addIcon from '../../assets/icons/add-24px.svg'
+import './HomePage.scss';
+import logo from '../../assets/images/logo.png'
+
+var employeeService = new EmployeeService();
+
+
+class HomePage extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            employeeArray: []
+
+        }
+    }
+
+    getEmployee = () => {
+
+        employeeService.getAllEmployee().then(obj => {
+            console.log("get data" + obj.data);
+            this.setState({ employeeArray: obj.data })
+
+        })
+            .catch(err => {
+                console.log("err while get");
+                this.setState({ employeeArray: [] })
+
+            })
+
+    }
+
+
+    openSearch() {
+
+    }
+    search() {
+
+    }
+
+    render() {
+        return (
+
+            <div onLoad={this.getEmployee}>
+                <header className='header row center'>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                    <div>
+                        <span className="emp-text">EMPLOYEE</span> <br />
+                        <span className="emp-text emp-payroll">PAYROLL</span>
+                    </div>
+                </div>
+            </header>
+                <div className="main-content">
+                    <div className="header-content">
+                        <div className="emp-detail-text">
+                            Employee Details <div className="emp-count"></div>
+                        </div>
+                        <div className="row center button box">
+                            <Link to="payroll-form" className="add-button flex-row-center">
+                                <img src={addIcon} alt="" />Add User
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="table-main" >
+                        <EmployeeTable employeeArray={this.state.employeeArray} />
+                    </div>
+                </div>
+            </div>
+        )
+
+    }
+
+}
+
+export default HomePage
