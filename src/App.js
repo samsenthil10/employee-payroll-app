efc883d91bc4f85df398f0b5651a355e96926cc2
Senthil Kumar S A <sasenthilk@gmail.com>
2021-11-13 06:56:05 +0530

    Added: To add route to home page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import PayrollForm from './components/payroll-form/PayrollForm'
+import HomePage from './components/home-page/HomePage';
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -6,12 +7,14 @@ import {
   Route
 } from "react-router-dom"
 
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/" element={<PayrollForm />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/payroll-form" element={<PayrollForm />} />
         </Routes>
       </Router>
     </div>
